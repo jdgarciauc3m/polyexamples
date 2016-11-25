21c2efd237f3e9e9a967c2325412750b2cf0ec7e
J. Daniel Garcia <jdaniel@arcos.inf.uc3m.es>
2016-11-25 20:17:29 +0100

    Code reorganization in shape

--- shapehgenopt/include/shape.h
+++ shapehgenopt/include/shape.h
@@ -11,10 +11,69 @@ namespace dsl { // Dummy Shapes Library
 class shape {
 private:
   class shape_base;
-  constexpr static int max_shape_size = sizeof(std::unique_ptr<shape_base>);
+  constexpr static int max_shape_size = 32; 
+
+  class shape_base {
+  public:
+    shape_base() noexcept {}
+    virtual ~shape_base() noexcept = default;
+
+    virtual shape_base * moving_clone(std::aligned_storage<max_shape_size>::type & buf) noexcept = 0;
+
+    virtual std::string classname() const = 0;
+    virtual int area() const noexcept = 0;
+    virtual void translate(int dx, int dy) noexcept = 0;
+    virtual void enlarge(int k) noexcept = 0;
+    virtual void insert(std::ostream & os) const noexcept = 0;
+    virtual void extract(std::istream & is) noexcept = 0;
+  };
+
+  template <typename S>
+  class concrete_shape : public shape_base {
+  public:
+    concrete_shape() noexcept : impl_{} {}
+    concrete_shape(S && x) noexcept : impl_{std::forward<S>(x)} {}
+    virtual ~concrete_shape() noexcept = default;
+
+    virtual shape_base * moving_clone(std::aligned_storage<max_shape_size>::type & buf) noexcept override {
+      return new (&buf) concrete_shape<S>(std::move(impl_));
+    }
+
+    std::string classname() const override { return impl_.classname(); }
+    int area() const noexcept override { return impl_.area(); }
+    void translate(int dx, int dy) noexcept override { impl_.translate(dx,dy); }
+    void enlarge(int k) noexcept override {impl_.enlarge(k); }
+    void insert(std::ostream & os) const noexcept override { os << impl_; }
+    void extract(std::istream & is) noexcept override { is >> impl_; }
+  private:
+    S impl_;
+  };
+  
+  template <typename S>
+  class dynamic_shape : public shape_base {
+  public:
+    dynamic_shape() noexcept : impl_{std::make_unique<S>()} {}
+    dynamic_shape(S && s) noexcept : impl_{std::make_unique<S>(std::forward<S>(s))} {}
+    dynamic_shape(std::unique_ptr<S> && p) noexcept : impl_{std::forward<std::unique_ptr<S>>(p)} {}
+
+    virtual ~dynamic_shape() noexcept = default;
+
+    virtual shape_base * moving_clone(std::aligned_storage<max_shape_size>::type & buf) noexcept override {
+      return new (&buf) dynamic_shape<S>(std::move(impl_));
+    }
+  
+    std::string classname() const noexcept override { return impl_->classname(); }
+    int area() const noexcept override { return impl_->area(); }
+    void translate(int dx, int dy) noexcept override { impl_->translate(dx,dy); }
+    void enlarge(int k) noexcept override {impl_->enlarge(k); }
+    void insert(std::ostream & os) const noexcept override { os << *impl_; }
+    void extract(std::istream & is) noexcept override { is >> *impl_; }
+  private:
+    std::unique_ptr<S> impl_;
+  };
 
   template <typename T>
-  static constexpr bool is_small() { return sizeof(T) <= max_shape_size; }
+  static constexpr bool is_small() { return sizeof(concrete_shape<T>) <= max_shape_size; }
 
   template <typename T>
   using small_shape = typename std::enable_if<is_small<T>(), shape>::type;
@@ -76,64 +135,6 @@ private:
     return reinterpret_cast<const shape_base*>(&buffer_); 
   }
 
-  class shape_base {
-  public:
-    shape_base() noexcept {}
-    virtual ~shape_base() noexcept = default;
-
-    virtual shape_base * moving_clone(std::aligned_storage<max_shape_size>::type & buf) noexcept = 0;
-
-    virtual std::string classname() const = 0;
-    virtual int area() const noexcept = 0;
-    virtual void translate(int dx, int dy) noexcept = 0;
-    virtual void enlarge(int k) noexcept = 0;
-    virtual void insert(std::ostream & os) const noexcept = 0;
-    virtual void extract(std::istream & is) noexcept = 0;
-  };
-
-  template <typename S>
-  class concrete_shape : public shape_base {
-  public:
-    concrete_shape() noexcept : impl_{} {}
-    concrete_shape(S && x) noexcept : impl_{std::forward<S>(x)} {}
-    virtual ~concrete_shape() noexcept = default;
-
-    virtual shape_base * moving_clone(std::aligned_storage<max_shape_size>::type & buf) noexcept override {
-      return new (&buf) concrete_shape<S>(std::move(impl_));
-    }
-
-    std::string classname() const override { return impl_.classname(); }
-    int area() const noexcept override { return impl_.area(); }
-    void translate(int dx, int dy) noexcept override { impl_.translate(dx,dy); }
-    void translate(int k) noexcept override {impl_.enlarge(k); }
-    void insert(std::ostream & os) const noexcept override { os << impl_; }
-    void extract(std::istream & is) noexcept override { is >> impl_; }
-  private:
-    S impl_;
-  };
-  
-  template <typename S>
-  class dynamic_shape : public shape_base {
-  public:
-    dynamic_shape() noexcept : impl_{std::make_unique<S>()} {}
-    dynamic_shape(S && s) noexcept : impl_{std::make_unique<S>(std::forward<S>(s))} {}
-    dynamic_shape(std::unique_ptr<S> && p) noexcept : impl_{std::forward<std::unique_ptr<S>>(p)} {}
-
-    virtual ~dynamic_shape() noexcept = default;
-
-    virtual shape_base * moving_clone(std::aligned_storage<max_shape_size>::type & buf) noexcept override {
-      return new (&buf) dynamic_shape<S>(std::move(impl_));
-    }
-  
-    std::string classname() const noexcept override { return impl_->classname(); }
-    int area() const noexcept override { return impl_->area(); }
-    void translate(int dx, int dy) noexcept override { impl_->translate(dx,dy); }
-    void enlarge(int k) noexcept override {impl_->enlarge(k); }
-    void insert(std::ostream & os) const noexcept override { os << *impl_; }
-    void extract(std::istream & is) noexcept override { is >> *impl_; }
-  private:
-    std::unique_ptr<S> impl_;
-  };
 
 public:
   template <typename S>
